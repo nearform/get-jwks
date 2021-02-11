ef668be04109e6fe1931f335d173abf98070f7c7
Filippo De Santis <filippo.desantis@nearform.com>
2021-02-11 09:37:02 +0100

    Add code and test for fetching JWKS

---
+++ src/jwks-fetch.js
@@ -0,0 +1,51 @@
+'use strict'
+
+const fetch = require('node-fetch')
+const MISSING_KEY_ERROR = 'No matching key found in the set.'
+
+async function jwksFetch ({ domain, alg, kid, cache = null }) {
+  const cacheKey = `${alg}:${kid}:${domain}`
+
+  const cached = cache ? cache.get(cacheKey) : null
+
+  if (cached) {
+    return cached
+  } else if (cache && cached === null) {
+    // null is returned when a previous attempt resulted in the key missing in the JWKs - Do not attemp to fetch again
+    throw new Error(MISSING_KEY_ERROR)
+  }
+
+  // Hit the well-known URL in order to get the key
+  const response = await fetch(`${domain}.well-known/jwks.json`, { timeout: 5000 })
+  const body = await response.json()
+
+  if (!response.ok) {
+    const error = new Error(response.statusText)
+    error.response = response
+    error.body = body
+
+    throw error
+  }
+
+  // Find the key with ID and algorithm matching the JWT token header
+  const key = body.keys.find(k => k.alg === alg && k.kid === kid)
+
+  if (!key) {
+    // Mark the key as missing
+    cache && cache.set(cacheKey, null)
+    throw new Error(MISSING_KEY_ERROR)
+  }
+
+  // should we be using https://github.com/Brightspace/node-jwk-to-pem ??
+
+  // certToPEM extracted from https://github.com/auth0/node-jwks-rsa/blob/master/src/utils.js
+  const secret = `-----BEGIN CERTIFICATE-----\n${key.x5c[0]}\n-----END CERTIFICATE-----\n`
+
+  // Save the key in the cache
+  cache && cache.set(cacheKey, secret)
+  return secret
+}
+
+module.exports = {
+  jwksFetch
+}
---
+++ test/jwks.spec.js
@@ -0,0 +1,158 @@
+'use strict'
+
+const NodeCache = require('node-cache')
+const nock = require('nock')
+const t = require('tap')
+
+const { jwksFetch } = require('../src/jwks-fetch')
+
+const jwks = {
+  keys: [
+    {
+      alg: 'RS512',
+      kid: 'KEY',
+      x5c: ['UNUSED']
+    },
+    {
+      alg: 'RS256',
+      kid: 'KEY',
+      x5c: [
+        `
+MIIEnjCCAoYCCQCMoDmTYrlYFTANBgkqhkiG9w0BAQsFADARMQ8wDQYDVQQDDAZ1
+bnVzZWQwHhcNMTkxMTEyMTIzMjI0WhcNMTkxMjEyMTIzMjI0WjARMQ8wDQYDVQQD
+DAZ1bnVzZWQwggIiMA0GCSqGSIb3DQEBAQUAA4ICDwAwggIKAoICAQDK7ys6lJMZ
+X5kt7NfsJHKHA7QXxmoixVw2lEPuyY/n4wg73+9IcyHmWUseb1AGHyXN1dD6GkcI
+ujuFJdrzdsNuFsCQDB7YE0/ZH9sqBAp6A8qh42ZAG/A8VkMGkMzSypvEcinJ7USO
+zYv9Q3BqKEAX41uE5dMRMVNQDEcHGxhoLwGpHECJgQ2NrRFK92WQvUuyJdoVF1hG
+WXSWAGfTZUHLpG3FTK3175we8qBsqynkvegAOwzETLdExWt620dRl7gRp6hDfECH
+69tdH6Qn1FC6fBKc1zvh79NA1iJrDCNJDFzN1bGVduPgOzsorhZSpt/ESw5YEOvC
+QAHOtzNmVa+4SOOm/2eDs5X066YmmRGv9aNC5humBPwfKFdIJbhCeP6XBaG2vtSx
+wfFEyfNCKoUTPUqdmj/CTW/TEFuzFab1hRLTmwOuLe2x3B0DuAkd/+auifXwDDPN
+GVs+VySqWeu00hSVEzKZ9FdU0abGkmRqytj7xw8gPJ+jroq5ZFAyPtPUf8IpSubX
+qAl0ppsqMrn9aMEEsu+APJi8yK4pEppWVZZBqf4/iPA+rR2J9uarUIsTQY8SKAeG
+BpcOTEjXvW5nTLmAE2hse39qrT5xWp/PXxmsMR6Q3Dn/drlySoNlCGIi4L+BfS+Q
+VZfm9BxIqS/aQW5TRMpeT7QAeK6NXD3dDwIDAQABMA0GCSqGSIb3DQEBCwUAA4IC
+AQCEwcGqCjW0FDrRepfglTLLk699SjidT8+DvnXEwhN85PFT4U0ArEe5n3Cb6ray
+qPEeOVG6QjLtGUZ9PRGVAjttfDQTAEWjqzJoqyAl60jj9Tm/G65UUbfHx37+Bvbc
+jlQ1FqZ4Jr4b14uFOONh0WH92VRDR47k/WWaP5bjxbyCIGcGzohh2XyrtOtDU+hV
+BntQ0w7736bL/MSunXO8tkx+LyM/Z4+HSWiwI+fcdIib27ZVFQ3W1NnRoufsSUqo
+Noi2XJqr1oLbSGpagLiXsIr8UufOrpZ92Pool0/B4y/d6GbbK2UjxyHjGKB8fwNi
+nU/+KAI1jPJT9dSc18u6F+cz4lQkGA9hmvApmiR7tTdcBWK/+m1lOHj4H8kZ2P/H
+fZuOj1+GtJ+JTZO35d+GPJ41NVLDAm5gc3kGkDPt+XRZZLAtafPhMGK7jUzEgyLI
+MITSqxjlBT++5VV035m84N+j5XJ0rYEHvgOmWJpJN+q/nIJpidq/6HzLOLoqmM5D
+UGiOoOTZIj3/OfyolcYztNb+rYe3Ch/KbReC/h1sU/xqLJCImDyhHwSarjDdi8A3
+dWxawCwETuA17mD7o/hsRUbXM6DHZekkuWPOL25UpRzlA1dtXMQ2ac83k+U6wyRs
+7jYWkrpLCTpEJcQ0uGEQnsTsjr2oCq/KvNmDki+iMtvjhA==
+        `.trim()
+      ]
+    }
+  ]
+}
+
+t.beforeEach((done, t) => {
+  nock.disableNetConnect()
+  done()
+})
+
+t.afterEach((done, t) => {
+  nock.cleanAll()
+  nock.enableNetConnect()
+  done()
+})
+
+t.test('should fetch the remove jwks and return an error if the request fails', async t => {
+  t.plan(2)
+  nock('https://localhost/').get('/.well-known/jwks.json').reply(500, { msg: 'no good' })
+  try {
+    await jwksFetch({ domain: 'https://localhost/', alg: 'RS512', kid: 'KEY' })
+  } catch (e) {
+    t.equal(e.message, 'Internal Server Error')
+    t.same(e.body, { msg: 'no good' })
+  }
+  t.end()
+})
+
+t.test('should fetch the remove jwks and return an error if alg and kid do not match', async t => {
+  t.plan(1)
+  nock('https://localhost/').get('/.well-known/jwks.json').reply(200, jwks)
+  try {
+    await jwksFetch({ domain: 'https://localhost/', alg: 'ABC', kid: 'some other KEY' })
+  } catch (e) {
+    t.equal(e.message, 'No matching key found in the set.')
+  }
+  t.end()
+})
+
+t.test('should fetch the remove jwks and return an the secrete if alg and kid match', async t => {
+  t.plan(2)
+  nock('https://localhost/').get('/.well-known/jwks.json').reply(200, jwks)
+  const secret = await jwksFetch({ domain: 'https://localhost/', alg: 'RS512', kid: 'KEY' })
+  t.ok(secret)
+  t.includes(secret, jwks.keys[0].x5c[0])
+  t.end()
+})
+
+t.test('if a cache is present and the cached key is null it whould return an error', async t => {
+  t.plan(1)
+  const domain = 'https://localhost/'
+  const alg = 'ABC'
+  const kid = 'key'
+  const cache = new NodeCache()
+  cache.set(`${alg}:${kid}:${domain}`, null)
+
+  try {
+    await jwksFetch({ domain, alg, kid, cache })
+  } catch (e) {
+    t.equal(e.message, 'No matching key found in the set.')
+  }
+  t.end()
+})
+
+t.test('if a cache is present and alg and kid do not match any jwks, the cache key should be set to null', async t => {
+  t.plan(2)
+  nock('https://localhost/').get('/.well-known/jwks.json').reply(200, jwks)
+  const domain = 'https://localhost/'
+  const alg = 'ALG'
+  const kid = 'KEY'
+  const cache = new NodeCache()
+
+  try {
+    await jwksFetch({ domain, alg, kid, cache })
+  } catch (e) {
+    t.equal(e.message, 'No matching key found in the set.')
+  }
+
+  t.equal(cache.get(`${alg}:${kid}:${domain}`), null)
+  t.end()
+})
+
+t.test('if a cache is present and the cached key is undefined it should fetch the jwks and set the key to the secret', async t => {
+  t.plan(3)
+  nock('https://localhost/').get('/.well-known/jwks.json').reply(200, jwks)
+  const domain = 'https://localhost/'
+  const alg = 'RS512'
+  const kid = 'KEY'
+  const cache = new NodeCache()
+
+  const secret = await jwksFetch({ domain, alg, kid, cache })
+  t.ok(secret)
+  t.includes(secret, jwks.keys[0].x5c[0])
+  t.equal(cache.get(`${alg}:${kid}:${domain}`), secret)
+  t.end()
+})
+
+t.test('if a cache is present and the cached key has a value it should return that value', async t => {
+  t.plan(3)
+  nock('https://localhost/').get('/.well-known/jwks.json').reply(200, jwks)
+  const domain = 'https://localhost/'
+  const alg = 'RS512'
+  const kid = 'KEY'
+  const cache = new NodeCache()
+  cache.set(`${alg}:${kid}:${domain}`, 'super secret')
+
+  const secret = await jwksFetch({ domain, alg, kid, cache })
+  t.ok(secret)
+  t.includes(secret, 'super secret')
+  t.equal(cache.get(`${alg}:${kid}:${domain}`), 'super secret')
+  t.end()
+})
