e26bb5747d5060ef53d65b0348c206cf78677740
Radomir Drndarski <90759187+radomird@users.noreply.github.com>
2022-02-24 10:55:57 +0100

    Added type definitions and tests for types (#115)
    * added type defs
    
    * Fix merge conflict
    
    * Fixed merge conflict
    
    Co-authored-by: Nate Totten <nate@zuplo.com>

---
+++ src/get-jwks.d.ts
@@ -0,0 +1,29 @@
+import LRU from 'lru-cache'
+
+type JWKSignature = { domain: string; alg: string; kid: string }
+type JWK = { [key: string]: any; domain: string; alg: string; kid: string }
+
+type GetPublicKeyOptions = {
+  domain?: string
+  alg?: string
+  kid?: string
+}
+
+type GetJwksOptions = {
+  max?: number
+  maxAge?: number
+  allowedDomains?: string[]
+  providerDiscovery?: boolean
+  jwksPath?: string
+  agent?: string
+}
+
+type GetJwks = {
+  getPublicKey: (options?: GetPublicKeyOptions) => Promise<string>
+  getJwk: (signature: JWKSignature) => Promise<JWK>
+  getJwksUri: (normalizedDomain: string) => Promise<string>
+  cache: LRU<string, JWK>
+  staleCache: LRU<string, JWK>
+}
+
+export default function buildGetJwks(options?: GetJwksOptions): GetJwks
