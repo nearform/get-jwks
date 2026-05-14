72a8233f4a86a54f18e41484bebed2b01152cfff
Luca Del Puppo <Puppo@users.noreply.github.com>
2026-05-14 08:34:51 +0200

    refactor(types): update type assertions in get-jwks tests

--- test/types/get-jwks.tst.ts
+++ test/types/get-jwks.tst.ts
@@ -5,9 +5,9 @@ import * as undici from "undici-types";
 
 const { getPublicKey, getJwk, getJwksUri, cache, staleCache } = buildGetJwks()
 
-expect(getJwk).type.toBeAssignableTo<(signature: JWKSignature) => Promise<JWK>>()
-expect(getJwksUri).type.toBeAssignableTo<(normalizedDomain: string) => Promise<string>>()
-expect(getPublicKey).type.toBeAssignableTo<(options?: GetPublicKeyOptions) => Promise<string>>()
-expect(cache).type.toBeAssignableTo<Cache<string, JWK>>()
-expect(staleCache).type.toBeAssignableTo<Cache<string, JWK>>()
+expect(getJwk).type.toBe<(signature: JWKSignature) => Promise<JWK>>()
+expect(getJwksUri).type.toBe<(normalizedDomain: string) => Promise<string>>()
+expect(getPublicKey).type.toBe<(options?: GetPublicKeyOptions) => Promise<string>>()
+expect(cache).type.toBe<Cache<string, JWK>>()
+expect(staleCache).type.toBe<Cache<string, JWK>>()
 expect({ dispatcher: undici.getGlobalDispatcher() }).type.toBeAssignableTo<GetJwksOptions['fetchOptions']>()
